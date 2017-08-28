053f8747a455185223a2a16ca7ee83b4e51fc8b7
SofiaIsabelle <sofia.i.rahimi@gmail.com>
2017-08-28 11:47:47 +0000

    Complete authentication system

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
   
   before_action :set_article, only:[:edit, :update, :show, :destroy]
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
   
    
   def index
@@ -18,13 +20,13 @@ class ArticlesController < ApplicationController
    
    def create
     @article = Article.new(article_params)
-    @article.user = User.first 
-    if @article.save
+    @article.user = current_user
+     if @article.save
     flash[:success] = "Article was successfully created"
     redirect_to article_path(@article)
-  else
+     else
     render 'new'
-    end
+     end
    
    end 
    
@@ -59,5 +61,13 @@ class ArticlesController < ApplicationController
    def article_params
       params.require(:article).permit(:title, :description)
    end   
+   
+   
+   def require_same_user
+    if current_user != @article.user 
+    flash[:danger] = "You can only edit or delete your own articles" 
+    redirect_to root_path
+    end
+   end 
    
 end    
